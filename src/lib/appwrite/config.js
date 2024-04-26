2455ad6dda526f921bdeef405aef90cbb6771329
andrijaweb <andrijawebd@gmail.com>
2024-04-26 22:27:22 +0200

    PostOperations added (like and save)

--- src/lib/appwrite/config.js
+++ src/lib/appwrite/config.js
@@ -7,7 +7,7 @@ export const appwriteConfig = {
   storageId: import.meta.env.VITE_APPWRITE_STORAGE_ID,
   userCollectionId: import.meta.env.VITE_APPWRITE_USER_COLLECTION_ID,
   postCollectionId: import.meta.env.VITE_APPWRITE_POST_COLLECTION_ID,
-  savedCollectionId: import.meta.env.VITE_APPWRITE_SAVED_SAVED_ID,
+  savedCollectionId: import.meta.env.VITE_APPWRITE_SAVED_COLLECTION_ID,
 };
 
 export const client = new Client();
