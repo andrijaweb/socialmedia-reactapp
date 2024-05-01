547a01ee64f4f2fb90a3ae5269fec661698523ab
andrijaweb <andrijawebd@gmail.com>
2024-05-01 20:44:45 +0200

    Masonry effect added to explore page

--- src/components/shared/PostListItem.jsx
+++ src/components/shared/PostListItem.jsx
@@ -1,49 +1,42 @@
 import { Link } from "react-router-dom";
 import { useUser } from "../../context/AuthContext";
+import { formatDate } from "../../lib/utils";
+import PostOperations from "./PostOperations";
 
 const PostListItem = ({ post }) => {
   const {
     user: { id: creatorId, name: creatorName, imageUrl: creatorAvatarUrl },
   } = useUser();
 
-  const { imageUrl, tags, caption } = post;
+  const { imageUrl, $updatedAt: postUpdatedAt } = post;
+
+  const postFormatedDate = formatDate(postUpdatedAt);
 
   return (
-    <li>
-      <>
-        <Link to="/">
-          <img
-            src={imageUrl}
-            alt="thumbnail"
-            className="rounded-md w-full object-cover max-h-80"
-          />
-        </Link>
+    <li className="list-none">
+      <Link to="/">
+        <img src={imageUrl} alt="thumbnail" className="rounded-md" />
+      </Link>
 
-        <div className="bg-dark-2 py-3 px-4">
-          <figure className="flex items-center gap-4 pt-2.5">
-            <Link to={`/profile/${creatorId}`}>
-              <div className="flex items-center gap-4">
-                <img
-                  src={creatorAvatarUrl}
-                  alt="avatar"
-                  className="h-12 w-12 rounded-full"
-                />
-              </div>
-            </Link>
-            <div>
-              <p>{creatorName}</p>
+      <div className="flex items-center justify-between py-4">
+        <figure className="flex items-center gap-4">
+          <Link to={`/profile/${creatorId}`}>
+            <div className="flex items-center gap-4">
+              <img
+                src={creatorAvatarUrl}
+                alt="avatar"
+                className="h-8 w-8 rounded-full"
+              />
             </div>
-          </figure>
-          <div className="flex items-center gap-2.5 mt-5 mb-2.5">
-            {tags.map((tag) => (
-              <p key={tag} className="text-primary-blue text-sm">
-                #{tag}
-              </p>
-            ))}
+          </Link>
+          <div>
+            <p className="text-sm">{creatorName}</p>
+            <p className="text-xs text-light-2">{postFormatedDate}</p>
           </div>
-          <p className="text-light-2 text-sm">{caption}</p>
-        </div>
-      </>
+        </figure>
+
+        <PostOperations post={post} userId={creatorId} />
+      </div>
     </li>
   );
 };
--- src/components/shared/PostsList.jsx
+++ src/components/shared/PostsList.jsx
@@ -1,13 +1,16 @@
+import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
 import PostListItem from "./PostListItem";
 
 const PostsList = ({ posts }) => {
   console.log(posts);
   return (
-    <ul className="grid grid-cols-3 mt-16 w-full gap-4">
-      {posts.map((post) => (
-        <PostListItem key={post.$id} post={post} />
-      ))}
-    </ul>
+    <ResponsiveMasonry>
+      <Masonry gutter="1rem">
+        {posts.map((post) => (
+          <PostListItem key={post.$id} post={post} />
+        ))}
+      </Masonry>
+    </ResponsiveMasonry>
   );
 };
 
