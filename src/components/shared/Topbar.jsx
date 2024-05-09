e427fd66563bd91f09d8e6527b4c78b763fbdd22
andrijaweb <andrijawebd@gmail.com>
2024-05-09 22:44:36 +0200

    Added bell icon

--- src/components/shared/Topbar.jsx
+++ src/components/shared/Topbar.jsx
@@ -4,7 +4,10 @@ import { Link, useNavigate } from "react-router-dom";
 import { useUser } from "../../context/AuthContext";
 import { useLogoutAccount } from "../../lib/react-query/authQueriesAndMutations";
 
-import { HiOutlineArrowRightStartOnRectangle } from "react-icons/hi2";
+import {
+  HiOutlineArrowRightStartOnRectangle,
+  HiOutlineBell,
+} from "react-icons/hi2";
 
 const Topbar = () => {
   const navigate = useNavigate();
@@ -28,6 +31,10 @@ const Topbar = () => {
             <img src={imageUrl} alt="avatar" className="h-8 w-8 rounded-full" />
           </div>
         </Link>
+
+        <button className="p-2.5 hover:bg-primary-blue-30 rounded-full">
+          <HiOutlineBell className="text-primary-blue text-2xl" />
+        </button>
 
         <button
           className="p-2.5 hover:bg-primary-blue-30 rounded-full"
