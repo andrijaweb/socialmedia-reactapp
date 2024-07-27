1da86fdd82adc63ae7156e1d3f4a4064750306c6
andrijaweb <andrijawebd@gmail.com>
2024-07-27 20:32:55 +0200

    Removed imgs folder, edited image paths, etc.

--- src/lib/appwrite/config.js
+++ src/lib/appwrite/config.js
@@ -5,6 +5,7 @@ export const appwriteConfig = {
   projectId: import.meta.env.VITE_APPWRITE_PROJECT_ID,
   databaseId: import.meta.env.VITE_APPWRITE_DATABASE_ID,
   storageId: import.meta.env.VITE_APPWRITE_STORAGE_ID,
+  storageFilesId: import.meta.env.VITE_APPWRITE_STORAGE_FILES_ID,
   userCollectionId: import.meta.env.VITE_APPWRITE_USER_COLLECTION_ID,
   postCollectionId: import.meta.env.VITE_APPWRITE_POST_COLLECTION_ID,
   savedCollectionId: import.meta.env.VITE_APPWRITE_SAVED_COLLECTION_ID,
